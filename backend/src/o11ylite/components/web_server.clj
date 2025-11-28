3d4a04be549852af3b2b49699784fcf5353cb86f
Zhiming Guo <guo.zhming@gmail.com>
2025-11-29 10:44:09 +1100

    rename server + virtual thread

---
+++ backend/src/o11ylite/components/web_server.clj
@@ -0,0 +1,34 @@
+;; ---------------------------------------------------------
+;; o11ylite.components.web-server
+;;
+;; Jetty web server component
+;; ---------------------------------------------------------
+
+(ns o11ylite.components.web-server
+  (:require
+   [integrant.core :as ig]
+   [com.brunobonacci.mulog :as mulog]
+   [ring.adapter.jetty :as jetty])
+  (:import
+   [org.eclipse.jetty.server Server]
+   [org.eclipse.jetty.util.thread QueuedThreadPool]))
+
+(defn- virtual-thread-pool
+  "Create a QueuedThreadPool that uses virtual threads."
+  []
+  (doto (QueuedThreadPool.)
+    (.setVirtualThreadsExecutor (java.util.concurrent.Executors/newVirtualThreadPerTaskExecutor))))
+
+(defmethod ig/init-key :server/web
+  [_ {:keys [host port handler]}]
+  (mulog/log ::web-server-starting :host host :port port)
+  (jetty/run-jetty handler
+                   {:host host
+                    :port port
+                    :join? false
+                    :thread-pool (virtual-thread-pool)}))
+
+(defmethod ig/halt-key! :server/web
+  [_ ^Server server]
+  (mulog/log ::web-server-stopping)
+  (.stop server))
