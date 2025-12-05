66a33fb9286b87cf1598761904920b73df371474
Zhiming Guo <guo.zhming@gmail.com>
2025-12-06 00:50:03 +1100

    event metadata cache component

---
+++ backend/src/o11ylite/components/event_metadata.clj
@@ -0,0 +1,121 @@
+;; ---------------------------------------------------------
+;; o11ylite.components.event-metadata
+;;
+;; Event metadata cache component.
+;; Caches field metadata (name, type) from the events table.
+;; Initialized after storage init, supports async refresh.
+;; ---------------------------------------------------------
+
+(ns o11ylite.components.event-metadata
+  (:require
+   [integrant.core :as ig]
+   [com.brunobonacci.mulog :as mulog]
+   [o11ylite.ducklake.schema :as schema]))
+
+;; ---------------------------------------------------------
+;; Public API
+
+(defn get-fields
+  "Get the current cached field metadata.
+   Returns a map of field-name -> {:type normalized-type}."
+  [event-metadata]
+  @(:state event-metadata))
+
+(defn get-field
+  "Get metadata for a specific field.
+   Returns {:type normalized-type} or nil if field doesn't exist."
+  [event-metadata field-name]
+  (get @(:state event-metadata) field-name))
+
+(defn refresh!
+  "Trigger an async refresh of the field metadata cache.
+   Returns a promise that will be delivered with:
+     - {:ok true :fields <field-map>} on success
+     - {:ok false :error <exception>} on failure
+   
+   The cache is only updated on successful refresh."
+  [event-metadata]
+  ((:refresh! event-metadata)))
+
+;; ---------------------------------------------------------
+;; Private Helpers
+
+(defn- -make-refresh-fn
+  "Create a refresh function that closes over duckdb and state."
+  [duckdb state]
+  (fn []
+    (let [p (promise)]
+      (future
+        (try
+          (let [fields (schema/fetch-event-fields duckdb)]
+            (reset! state fields)
+            (mulog/log ::event-metadata-refreshed :field-count (count fields))
+            (deliver p {:ok true :fields fields}))
+          (catch Exception e
+            (mulog/log ::event-metadata-refresh-failed :error (.getMessage e))
+            (deliver p {:ok false :error e}))))
+      p)))
+
+;; ---------------------------------------------------------
+;; Component Lifecycle
+
+(defmethod ig/init-key :cache/event-metadata
+  [_ {:keys [duckdb]}]
+  (mulog/log ::event-metadata-starting)
+  (let [state (atom {})
+        fields (schema/fetch-event-fields duckdb)]
+    (reset! state fields)
+    (mulog/log ::event-metadata-started :field-count (count fields))
+    {:state state
+     :refresh! (-make-refresh-fn duckdb state)}))
+
+(defmethod ig/halt-key! :cache/event-metadata
+  [_ _]
+  ;; Nothing to clean up
+  nil)
+
+;; ---------------------------------------------------------
+;; Rich Comment
+(comment
+
+  ;; Test event metadata manually
+  (require '[integrant.core :as ig])
+
+  ;; Start dependencies
+  (def duckdb-ds
+    (ig/init-key :db/duckdb {:data-path "./.tmp"}))
+
+  (def sqlite-ds
+    (ig/init-key :db/sqlite {:data-path "./.tmp"}))
+
+  ;; Initialize storage (creates events table)
+  (def storage
+    (ig/init-key :storage/init {:sqlite sqlite-ds :duckdb duckdb-ds}))
+
+  ;; Start event metadata component
+  (def em
+    (ig/init-key :cache/event-metadata {:duckdb duckdb-ds}))
+
+  ;; Get all fields (now with normalized types)
+  (get-fields em)
+  ;; => {"service" {:type :string}
+  ;;     "timestamp" {:type :instant}
+  ;;     "span.duration_ns" {:type :integer}
+  ;;     ...}
+
+  ;; Get specific field
+  (get-field em "service")      ;; => {:type :string}
+  (get-field em "timestamp")    ;; => {:type :instant}
+  (get-field em "span.duration_ns")  ;; => {:type :integer}
+
+  ;; Async refresh
+  @(refresh! em)
+
+  ;; Cleanup
+  (ig/halt-key! :cache/event-metadata em)
+  (ig/halt-key! :storage/init storage)
+  (ig/halt-key! :db/sqlite sqlite-ds)
+  (ig/halt-key! :db/duckdb duckdb-ds)
+
+  #_()) ; End of rich comment block
+;; ---------------------------------------------------------
---
+++ backend/test/o11ylite/integration/event_metadata_test.clj
@@ -0,0 +1,72 @@
+;; ---------------------------------------------------------
+;; o11ylite.integration.event-metadata-test
+;;
+;; Integration tests for the event metadata cache component.
+;; ---------------------------------------------------------
+
+(ns o11ylite.integration.event-metadata-test
+  (:require
+   [clojure.test :refer [deftest is testing use-fixtures]]
+   [o11ylite.test-helpers :as h]
+   [o11ylite.components.event-metadata :as event-metadata]))
+
+(use-fixtures :each h/with-system)
+
+;; ---------------------------------------------------------
+;; Helper to get event metadata component from system
+
+(defn- event-metadata-component []
+  (:cache/event-metadata h/*system*))
+
+;; ---------------------------------------------------------
+;; Tests
+
+(deftest event-metadata-initialized-test
+  (testing "Event metadata is populated after system start"
+    (let [em (event-metadata-component)
+          fields (event-metadata/get-fields em)]
+      (is (map? fields))
+      (is (pos? (count fields)) "Should have fields from events table"))))
+
+(deftest event-metadata-has-core-fields-test
+  (testing "Event metadata contains expected core fields"
+    (let [em (event-metadata-component)]
+      ;; Core identity fields
+      (is (some? (event-metadata/get-field em "service")))
+      (is (some? (event-metadata/get-field em "timestamp")))
+      ;; Trace context
+      (is (some? (event-metadata/get-field em "trace_id")))
+      (is (some? (event-metadata/get-field em "span_id")))
+      ;; Signal type
+      (is (some? (event-metadata/get-field em "meta.signal_type"))))))
+
+(deftest event-metadata-field-types-test
+  (testing "Event metadata has normalized application-level types"
+    (let [em (event-metadata-component)]
+      (is (= :string (:type (event-metadata/get-field em "service"))))
+      (is (= :instant (:type (event-metadata/get-field em "timestamp"))))
+      (is (= :integer (:type (event-metadata/get-field em "span.duration_ns")))))))
+
+(deftest event-metadata-refresh-async-test
+  (testing "Async refresh returns promise with fields"
+    (let [em (event-metadata-component)
+          p (event-metadata/refresh! em)
+          result (deref p 5000 {:ok false :error "timeout"})]
+      (is (:ok result))
+      (is (map? (:fields result)))
+      (is (pos? (count (:fields result)))))))
+
+(deftest event-metadata-get-nonexistent-field-test
+  (testing "Getting nonexistent field returns nil"
+    (let [em (event-metadata-component)]
+      (is (nil? (event-metadata/get-field em "nonexistent_field_xyz"))))))
+
+;; ---------------------------------------------------------
+;; Rich Comment
+(comment
+
+  (require '[clojure.test :refer [run-tests]])
+  (run-tests 'o11ylite.integration.event-metadata-test)
+
+  #_()) ; End of rich comment block
+;; ---------------------------------------------------------
