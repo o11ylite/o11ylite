e53befc1149ffa214e1d0dced592bada37b20ac6
Zhiming Guo <guo.zhming@gmail.com>
2026-01-10 21:09:08 +1100

    support metric ingestion by http

--- backend/test/o11ylite/integration/otel_http_test.clj
+++ backend/test/o11ylite/integration/otel_http_test.clj
@@ -145,6 +145,86 @@
           (is (= "proto-user-123" (:attr.user.id row))))))))
 
 ;; ---------------------------------------------------------
+;; Metric Tests - JSON
+
+(deftest otlp-metrics-json-test
+  (testing "POST /v1/metrics accepts JSON with empty resourceMetrics"
+    (let [response (http/post "/v1/metrics"
+                              {:headers {"Content-Type" "application/json"}
+                               :body "{\"resourceMetrics\":[]}"})]
+      (is (= 200 (:status response)))
+      (is (= "application/json" (get-in response [:headers "content-type"])))))
+
+  (testing "POST /v1/metrics accepts JSON with valid metric data"
+    (let [metric-json (h/->json
+                       {:resourceMetrics
+                        [{:resource {:attributes [{:key "service.name"
+                                                   :value {:stringValue "test-service"}}]}
+                          :scopeMetrics
+                          [{:scope {:name "test-scope"}
+                            :metrics [{:name "cpu.usage"
+                                       :description "CPU usage"
+                                       :unit "%"
+                                       :gauge {:dataPoints [{:timeUnixNano 1234567890000000000
+                                                             :asDouble 42.5}]}}]}]}]})
+          response (http/post "/v1/metrics"
+                              {:headers {"Content-Type" "application/json"}
+                               :body metric-json})]
+      (is (= 200 (:status response))))))
+
+;; ---------------------------------------------------------
+;; Metric Tests - Protobuf Binary
+
+(defn- query-metrics-by-name
+  "Query metrics from DuckLake by metric name."
+  [metric-name]
+  (jdbc/execute! (duckdb)
+                 ["SELECT * FROM o11ylite.metrics WHERE name = ?"
+                  metric-name]))
+
+(deftest otlp-metrics-protobuf-test
+  (testing "POST /v1/metrics accepts protobuf binary with valid metric data"
+    (let [service-name "http-proto-metric-test"
+          metric-name "http.proto.cpu.usage"
+          proto-request (otlp/build-metrics-request
+                         {:service-name service-name
+                          :meter-name "test-meter"
+                          :metrics [(otlp/build-gauge-metric
+                                     {:name metric-name
+                                      :description "CPU usage via HTTP protobuf"
+                                      :unit "%"
+                                      :data-points [{:value 65.5
+                                                     :attributes {"host.name" "http-test-server"}}]})]})
+          response (http/post "/v1/metrics"
+                              {:headers {"Content-Type" "application/x-protobuf"}
+                               :body (.toByteArray proto-request)})]
+      (is (= 200 (:status response)))
+      ;; Wait for batcher to flush (test config uses 100ms interval)
+      (Thread/sleep 200)
+      ;; Verify data was persisted
+      (let [rows (query-metrics-by-name metric-name)]
+        (is (= 1 (count rows)))
+        (let [row (first rows)]
+          (is (= metric-name (:name row)))
+          (is (= service-name (:service row)))
+          (is (= 65.5 (:value row)))
+          (is (= "http-test-server" (:attr.host.name row))))))))
+
+(deftest otlp-metrics-content-type-handling-test
+  (testing "Responds with JSON by default for metrics"
+    (let [response (http/post "/v1/metrics"
+                              {:headers {"Content-Type" "application/json"}
+                               :body "{\"resourceMetrics\":[]}"})]
+      (is (= "application/json" (get-in response [:headers "content-type"])))))
+
+  (testing "Responds with protobuf when Accept header requests it for metrics"
+    (let [response (http/post "/v1/metrics"
+                              {:headers {"Content-Type" "application/json"
+                                         "Accept" "application/x-protobuf"}
+                               :body "{\"resourceMetrics\":[]}"})]
+      (is (= "application/x-protobuf" (get-in response [:headers "content-type"]))))))
+
+;; ---------------------------------------------------------
 ;; Rich Comment
 (comment
 
@@ -152,6 +232,9 @@
   (k/run #'otlp-traces-json-test)
   (k/run #'otlp-logs-json-test)
   (k/run #'otlp-content-type-handling-test)
+  (k/run #'otlp-metrics-json-test)
+  (k/run #'otlp-metrics-protobuf-test)
+  (k/run #'otlp-metrics-content-type-handling-test)
 
   #_()) ; End of rich comment block
 ;; ---------------------------------------------------------
