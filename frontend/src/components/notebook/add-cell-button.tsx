29a2837f96ceda2f10bac241d9b14a6982909f2e
Zhiming Guo <guo.zhming@gmail.com>
2026-02-17 20:42:39 +1100

    fix notebook new cell button

--- frontend/src/components/notebook/add-cell-button.tsx
+++ frontend/src/components/notebook/add-cell-button.tsx
@@ -7,7 +7,7 @@ export function AddCellButton({ notebookId }: { notebookId: string }) {
   const handleAdd = () => {
     router.post(`/notebooks/${notebookId}/cells`, {
       query_mode: "events",
-      query: JSON.stringify({ visualization: { type: "table" } }),
+      query: { visualization: { type: "table" } },
     })
   }
 
